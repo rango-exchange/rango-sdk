88e9ba6b55eb5ebc0756d6301a463ae4f25a2fd9
RanGojo <rangojo@rango.exchange>
2022-03-09 11:30:45 +0330

    update documents

--- src/types/api/routing.ts
+++ src/types/api/routing.ts
@@ -83,13 +83,14 @@ export type BlockchainValidationStatus = {
  * and true when user really accepted to swap. If true, server will be much slower to respond, but will check some
  * pre-requisites including balance of X and required fees in user's wallets
  * @property {{ [key: string]: string }} selectedWallets - Map of blockchain to selected address
+ * @property {UserWalletBlockchain[]} connectedWallets - List of all user connected wallet addresses per each blockchain
  * @property {string | null} [affiliateRef] - The affiliate ref that client likes to send to Rango, so in cases of
  * 1inch, Thorchain, etc. that support affiliation, the referrer will earn some money if user accept the route and
  * signs the transactions. example: K3ldk3
- * @property {boolean} [disableMultiStepTx] - It should be true when client doesn't want multi step transactions
- * @property {string[]} [blockchains] - The list of all accepted blockchains, empty list means no filter is required
- * @property {string[]} [swappers] - The list of all accepted swappers, empty list means no filter is required
- * @property {string[]} [transactionTypes] - The list of all accepted transaction types containing [EVM, TRANSFER, COSMOS]
+ * @property {boolean} [disableMultiStepTx] - It should be true when the client doesn't want multi-step transactions
+ * @property {string[]} [blockchains] - List of all accepted blockchains, an empty list means no filter is required
+ * @property {string[]} [swappers] - List of all accepted swappers, an empty list means no filter is required
+ * @property {string[]} [transactionTypes] - List of all accepted transaction types including [EVM, TRANSFER, COSMOS]
  *
  */
 export type BestRouteRequest = {
@@ -109,21 +110,21 @@ export type BestRouteRequest = {
 /**
  * The response of best route, if the result fields is null, it means that no route is found
  *
- * @property {string} requestId - The unique requestId generated for this request by server. It should be passed down
- * to all other endpoints if this swap continues on, example: d10657ce-b13a-405c-825b-b47f8a5016ad
- * @property {string} requestAmount - The human readable amount from the request
+ * @property {string} requestId - The unique requestId which is generated for this request by the server. It should be
+ * passed down to all other endpoints if this swap continues on. e.g. d10657ce-b13a-405c-825b-b47f8a5016ad
+ * @property {string} requestAmount - The human readable input amount from the request
  * @property {Asset} from
  * @property {Asset} to
  * @property {SimulationResult | null} result
- * @property {BlockchainValidationStatus[] | null} validationStatus - Pre-requisite check result, it will be null if
- * request.checkPrerequisites is false
+ * @property {BlockchainValidationStatus[] | null} validationStatus - Pre-requisites check result. It will be null if
+ * the request checkPrerequisites was false
  * @property {string[]} diagnosisMessages - list of string messages that might be cause of not finding the route.
- * It's just for display purpose
+ * It's just for display purposes
  * @property {string[]} missingBlockchains - List of all blockchains which are necessary to be present for the best
- * route and user has not provided any connected wallets for it. Null or empty list indicates that there is no problem.
- * @property {boolean} processingLimitReached - A warning that indicates that it took too much time to find the best
- * route and server could not find any routes from X to Y
- * @property {boolean} walletNotSupportingFromBlockchain - A warning indicating that none of your wallets have the same
+ * route and user has not provided any connected wallets for it. A null or empty list indicates that there is no problem.
+ * @property {boolean} processingLimitReached - A warning indicates that it took too much time to find the best
+ * route and the server could not find any routes from X to Y
+ * @property {boolean} walletNotSupportingFromBlockchain - A warning indicates that none of your wallets have the same
  * blockchain as X asset
  *
  */
