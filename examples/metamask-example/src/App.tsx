ee8342e2e76125b9d55124224811a664e02e75ed
RanGojo <rangojo@rango.exchange>
2022-05-08 22:53:37 +0430

    updating metamask example

--- examples/metamask-example/src/App.tsx
+++ examples/metamask-example/src/App.tsx
@@ -2,15 +2,17 @@ import './App.css';
 import {useEffect, useState} from "react"
 import {ethers} from 'ethers'
 import {
-  BestRouteResponse,
   EvmTransaction,
   MetaResponse,
   RangoClient,
   TransactionStatus,
-  TransactionStatusResponse,
-  WalletRequiredAssets
-} from "rango-sdk"
-import {checkApprovalSync, prepareEvmTransaction, prettyAmount, sleep} from "./utils";
+  QuoteResponse,
+  StatusResponse,
+  Asset
+} from "rango-sdk-basic/lib"
+import {prepareEvmTransaction, sleep} from "./utils";
+import BigNumber from "bignumber.js";
+import React from 'react';
 
 declare let window: any
 
@@ -21,9 +23,8 @@ export const App = () => {
   const rangoClient = new RangoClient(RANGO_API_KEY)
   const [tokensMeta, setTokenMeta] = useState<MetaResponse | null>()
   const [inputAmount, setInputAmount] = useState<string>("0.1")
-  const [bestRoute, setBestRoute] = useState<BestRouteResponse | null>()
-  const [txStatus, setTxStatus] = useState<TransactionStatusResponse | null>(null)
-  const [requiredAssets, setRequiredAssets] = useState<WalletRequiredAssets[]>([])
+  const [quote, setQuote] = useState<QuoteResponse | null>()
+  const [txStatus, setTxStatus] = useState<StatusResponse | null>(null)
   const [loadingMeta, setLoadingMeta] = useState<boolean>(true)
   const [loadingSwap, setLoadingSwap] = useState<boolean>(false)
   const [error, setError] = useState<string>("")
@@ -31,15 +32,35 @@ export const App = () => {
   useEffect(() => {
     setLoadingMeta(true)
     // Meta provides all blockchains, tokens and swappers information supported by Rango
-    rangoClient.getAllMetadata().then((meta) => {
+    rangoClient.meta().then((meta) => {
       setTokenMeta(meta)
       setLoadingMeta(false)
     })
   }, [])
 
   const usdtAddressInPolygon = '0xc2132d05d31c914a87c6611c10748aeb04b58e8f'
-  const usdtToken = tokensMeta?.tokens.find(t => t.blockchain === "POLYGON" && t.address === usdtAddressInPolygon)
-  const maticToken = tokensMeta?.tokens.find(t => t.blockchain === "POLYGON" && t.address === null)
+  const usdtAddressInBSC = '0x55d398326f99059ff775485246999027b3197955'
+  const usdtAddressInFantom = '0x049d68029688eabf473097a2fc38ef61633a3c7a'
+
+  // 1inch sample: POLYGON.USDT -> POLYGON.MATIC
+  // const sourceChainId = 137
+  // const sourceToken = tokensMeta?.tokens.find(t => t.blockchain === "POLYGON" && t.address === usdtAddressInPolygon)
+  // const destinationToken = tokensMeta?.tokens.find(t => t.blockchain === "POLYGON" && t.address === null)
+
+  // anyswap sample: POLYGON.USDT to BSC.USDT
+  // const sourceChainId = 137
+  // const sourceToken = tokensMeta?.tokens.find(t => t.blockchain === "POLYGON" && t.address === usdtAddressInPolygon)
+  // const destinationToken = tokensMeta?.tokens.find(t => t.blockchain === "BSC" && t.address === usdtAddressInBSC)
+
+  // aggregator sample 1 BSC.BNB to FTM.USDT
+  // const sourceChainId = 56
+  // const sourceToken = tokensMeta?.tokens.find(t => t.blockchain === "BSC" && t.address === null)
+  // const destinationToken = tokensMeta?.tokens.find(t => t.blockchain === "FANTOM" && t.address === usdtAddressInFantom)
+
+  // aggregator sample 2 BSC.BNB to FTM.FTM
+  const sourceChainId = 56
+  const sourceToken = tokensMeta?.tokens.find(t => t.blockchain === "BSC" && t.address === null)
+  const destinationToken = tokensMeta?.tokens.find(t => t.blockchain === "FANTOM" && t.address === null)
 
 
   const getUserWallet = async () => {
@@ -50,12 +71,12 @@ export const App = () => {
 
   const swap = async () => {
     setError("")
-    setBestRoute(null)
+    setQuote(null)
     setTxStatus(null)
-    setRequiredAssets([])
     let userAddress = ''
     try {
       userAddress = await getUserWallet()
+      console.log({userAddress})
     } catch (err) {
       setError('Error connecting to MetMask. Please check Metamask and try again.')
       return
@@ -66,9 +87,8 @@ export const App = () => {
       return
     }
 
-    // it multi swap example, you should check chain id before each evm swap
-    if (window.ethereum.chainId && parseInt(window.ethereum.chainId) !== 137) {
-      setError(`Change meta mask network to 'Polygon'.`)
+    if (window.ethereum.chainId && parseInt(window.ethereum.chainId) !== sourceChainId) {
+      setError(`Change meta mask network to '${sourceToken?.blockchain}'.`)
       return
     }
 
@@ -80,119 +100,116 @@ export const App = () => {
       setError(`Set input amount`)
       return
     }
-    if (!usdtToken || !maticToken)
+    if (!sourceToken || !destinationToken)
       return
 
     setLoadingSwap(true)
-    const connectedWallets = [{blockchain: 'POLYGON', addresses: [userAddress]}]
-    const selectedWallets = {'POLYGON': userAddress}
-    const from = {blockchain: usdtToken?.blockchain, symbol: usdtToken?.symbol, address: usdtToken.address}
-    const to = {blockchain: maticToken?.blockchain, symbol: maticToken?.symbol, address: maticToken.address}
+    const from: Asset = {blockchain: sourceToken?.blockchain, symbol: sourceToken?.symbol, address: sourceToken.address}
+    const to: Asset = {blockchain: destinationToken?.blockchain, symbol: destinationToken?.symbol, address: destinationToken.address}
+    const amount: string = (new BigNumber(inputAmount)).shiftedBy(sourceToken.decimals).toString()
 
-    // If you just want to show route to user, set checkPrerequisites: false.
-    // Also for multi steps swap, it is faster to get route first with {checkPrerequisites: false} and if users confirms.
-    // check his balance with {checkPrerequisites: true} in another get best route request
-    const bestRoute = await rangoClient.getBestRoute({
-      amount: inputAmount,
-      affiliateRef: null,
-      checkPrerequisites: true,
-      connectedWallets,
+    const quoteResponse = await rangoClient.quote({
+      amount,
       from,
-      selectedWallets,
       to,
     })
-    setBestRoute(bestRoute)
+    setQuote(quoteResponse)
+    console.log({quoteResponse})
 
-    console.log({bestRoute})
-    if (!bestRoute || !bestRoute?.result || !bestRoute?.result?.swaps || bestRoute.result?.swaps?.length === 0) {
-      setError(`Invalid route response from server, please try again.`)
+    if (!quoteResponse || !quoteResponse?.route || quoteResponse.resultType !== "OK") {
+      setError(`Invalid quote response: ${quoteResponse.resultType}, please try again.`)
       setLoadingSwap(false)
       return
     }
-
-    const requiredCoins =
-      bestRoute.validationStatus?.flatMap(v => v.wallets.flatMap(w => w.requiredAssets)) || []
-    setRequiredAssets(requiredCoins)
-    const hasEnoughBalance = requiredCoins?.map(it => it.ok).reduce((a, b) => a && b)
-
-    if (!hasEnoughBalance) {
-      setError(`Not enough balance or fee!`)
-      setLoadingSwap(false)
-      return
-    }
-    else if (bestRoute) {
-      await executeRoute(bestRoute)
+    else {
+      await executeRoute(from, to, userAddress, amount)
     }
   }
 
-  const executeRoute = async (routeResponse: BestRouteResponse) => {
+  const executeRoute = async (from: Asset, to: Asset, fromAddress: string, inputAmount: string) => {
     const provider = await new ethers.providers.Web3Provider(window.ethereum as any)
     const signer = provider.getSigner()
+    if (!sourceToken || !destinationToken)
+      return
 
-    // In multi step swaps, you should loop over routeResponse.route array and create transaction per each item
-    let evmTransaction
     try {
-      // A transaction might needs multiple approval txs (e.g. in harmony bridge),
-      // you should create transaction and check approval again and again until `isApprovalTx` field turns to false
-      while (true) {
-        const transactionResponse = await rangoClient.createTransaction({
-          requestId: routeResponse.requestId,
-          step: 1, // In this example, we assumed that route has only one step
-          userSettings: { 'slippage': '1' },
-          validations: { balance: true, fee: true },
-        })
+      const swapResponse = await rangoClient.swap({
+        from,
+        to,
+        amount: inputAmount,
+        fromAddress: fromAddress,
+        toAddress: fromAddress,
+        disableEstimate: false,
+        referrerAddress: null,
+        referrerFee: null,
+        slippage: '1.0'
+      })
+      console.log({swapResponse})
 
-        // in general case, you should check transaction type and call related provider to sign and send tx
-        evmTransaction = transactionResponse.transaction as EvmTransaction
-        if (evmTransaction.isApprovalTx) {
-          const finalTx = prepareEvmTransaction(evmTransaction)
-          await signer.sendTransaction(finalTx)
-          await checkApprovalSync(routeResponse, rangoClient)
-          console.log("transaction approved successfully")
-        }
-        else {
-          break
-        }
+      if (!!swapResponse.error || swapResponse.resultType === "NO_ROUTE" || swapResponse.resultType === "INPUT_LIMIT_ISSUE") {
+        setError(`Error swapping, error message: ${swapResponse.error}, result type: ${swapResponse.resultType}`)
+        setLoadingSwap(false)
+        return
       }
 
-      const finalTx = prepareEvmTransaction(evmTransaction)
-      const txHash = (await signer.sendTransaction(finalTx)).hash
-      const txStatus = await checkTransactionStatusSync(txHash, routeResponse, rangoClient)
-      console.log("transaction finished", {txStatus})
-      setLoadingSwap(false)
+      const evmTransaction = swapResponse.tx as EvmTransaction
+      console.log({evmTransaction})
+
+      if (!!evmTransaction.approveData) {
+        // try to approve
+      } else {
+        const finalTx = prepareEvmTransaction(evmTransaction)
+        const txHash = (await signer.sendTransaction(finalTx)).hash
+        const txStatus = await checkTransactionStatusSync(swapResponse.requestId, txHash, rangoClient)
+        console.log("transaction finished", {txStatus})
+        setLoadingSwap(false)
+      }
+
+        // in general case, you should check transaction type and call related provider to sign and send tx
+    //     evmTransaction = transactionResponse.transaction as EvmTransaction
+    //     if (evmTransaction.isApprovalTx) {
+    //       const finalTx = prepareEvmTransaction(evmTransaction)
+    //       await signer.sendTransaction(finalTx)
+    //       await checkApprovalSync(routeResponse, rangoClient)
+    //       console.log("transaction approved successfully")
+    //     }
+    //     else {
+    //       break
+    //     }
+    //   }
+    //
+    //   const finalTx = prepareEvmTransaction(evmTransaction)
+    //   const txHash = (await signer.sendTransaction(finalTx)).hash
+    //   const txStatus = await checkTransactionStatusSync(txHash, routeResponse, rangoClient)
+    //   console.log("transaction finished", {txStatus})
+    //   setLoadingSwap(false)
     } catch (e) {
       const rawMessage = JSON.stringify(e).substring(0, 90) + '...'
       setLoadingSwap(false)
       setError(rawMessage)
       // report transaction failure to server if something went wrong in client for signing and sending the transaction
-      await rangoClient.reportFailure({
-        data: { message: rawMessage },
-        eventType: 'TX_FAIL',
-        requestId: routeResponse.requestId,
-      })
+      // await rangoClient.reportFailure({
+      //   data: { message: rawMessage },
+      //   eventType: 'TX_FAIL',
+      //   requestId: routeResponse.requestId,
+      // })
     }
   }
 
-  const checkTransactionStatusSync = async (txHash: string, bestRoute: BestRouteResponse, rangoClient: RangoClient) => {
+  const checkTransactionStatusSync = async (requestId: string, txHash: string, rangoClient: RangoClient) => {
     while (true) {
-      const txStatus = await rangoClient.checkStatus({
-        requestId: bestRoute.requestId,
-        step: 1,
+      const txStatus = await rangoClient.status({
+        requestId: requestId,
         txId: txHash,
       })
       setTxStatus(txStatus)
       console.log({txStatus})
       if (!!txStatus.status && [TransactionStatus.FAILED, TransactionStatus.SUCCESS].includes(txStatus.status)) {
-        // for some swappers (e.g. harmony bridge), it needs more than one transaction to be signed for one single step
-        // swap. In these cases, you need to check txStatus.newTx and make sure it's null before going to the next step.
-        // If it's not null, you need to use that to create next transaction of this step.
         return txStatus
       }
       await sleep(3000)
     }
   }
-
-  const swapperLogo = tokensMeta?.swappers.find(sw => sw.id === bestRoute?.result?.swaps[0].swapperId)?.logo
 
   return (
     <div className="container">
@@ -202,7 +219,7 @@ export const App = () => {
       <div className="tokens-container">
         <div className="from">
           {loadingMeta && (<div className="loading"/>)}
-          {!loadingMeta && (<img src={usdtToken?.image} alt="USDT" height="50px"/>)}
+          {!loadingMeta && (<img src={sourceToken?.image} alt="USDT" height="50px"/>)}
           <div>
             <input
               type="number"
@@ -215,37 +232,39 @@ export const App = () => {
               step="0.01"
             />
           </div>
-          <div className="symbol">{usdtToken?.symbol}</div>
-          <div className="blockchain">from {usdtToken?.blockchain}</div>
+          <div className="symbol">{sourceToken?.symbol}</div>
+          <div className="blockchain">from {sourceToken?.blockchain}</div>
         </div>
         <div className="swap-details-container">
           <div className="swap-details">
-            {requiredAssets && (
-              <div>
-                Required Balance Check:
-                {requiredAssets.map((item, index) => (
-                  <div key={index}>+ {item.asset.symbol},
-                    type: {item.reason},
-                    required: {prettyAmount(item.requiredAmount)},
-                    current: {prettyAmount(item.currentAmount)},
-                    enough: {item.ok ? 'yes' : 'no'}
-                  </div>
-                ))}
-              </div>
-            )}
             <img src="./img/arrow.png" className="arrow" alt="to"/>
-            {bestRoute && <div className='green-text'>
-              {swapperLogo && (<img src={swapperLogo} alt="swapper logo" width={50}/>)} <br/>
-              {bestRoute?.result?.swaps[0].swapperId}
+            {quote && <div className='green-text'>
+              {quote.route?.swapper && (<img src={quote.route?.swapper?.logo} alt="swapper logo" width={50}/>)} <br/>
+              {quote.route?.swapper?.title}
             </div>}
-            {txStatus && (
+            {quote && (
               <div>
                 <br/>
-                <div>status: {txStatus.status || TransactionStatus.RUNNING}</div>
-                <div>details: {txStatus.extraMessage || '-'}</div>
-                {txStatus.explorerUrl?.map((item) => (
-                    <div><a href={item.url}>{item.description || "Tx Hash"}</a></div>
-                  )
+                {quote && (
+                  <React.Fragment>
+                    <div>expected output:
+                      {new BigNumber(quote?.route?.outputAmount || "0").shiftedBy(-(destinationToken?.decimals || 0)).toString()} {destinationToken?.symbol}
+                    </div>
+                    <div>time estimate:
+                      {quote.route?.estimatedTimeInSeconds}s
+                    </div>
+                  </React.Fragment>
+                )}
+                {txStatus && (
+                  <React.Fragment>
+                    <div>status: {txStatus.status || TransactionStatus.RUNNING}</div>
+                    <div>real output: {txStatus.output?.amount || '?'} {txStatus.output?.receivedToken?.symbol || ""}  {txStatus.output?.type || ""}</div>
+                    <div>details: {txStatus.error || '-'}</div>
+                    {txStatus.explorerUrl?.map((item, id) => (
+                        <div key={id}><a href={item.url}>{item.description || "Tx Hash"}</a></div>
+                      )
+                    )}
+                  </React.Fragment>
                 )}
                 <br/>
               </div>
@@ -256,10 +275,10 @@ export const App = () => {
         </div>
         <div className="to">
           {loadingMeta && (<div className="loading"/>)}
-          {!loadingMeta && (<img src={maticToken?.image} alt="Matic" height="50px"/>)}
-          <div className="amount green-text">{bestRoute?.result?.outputAmount || "?"}</div>
-          <div className="symbol">{maticToken?.symbol}</div>
-          <div className="blockchain">to {maticToken?.blockchain}</div>
+          {!loadingMeta && (<img src={destinationToken?.image} alt="Matic" height="50px"/>)}
+          {/*<div className="amount green-text">{bestRoute?.result?.outputAmount || "?"}</div>*/}
+          <div className="symbol">{destinationToken?.symbol}</div>
+          <div className="blockchain">to {destinationToken?.blockchain}</div>
         </div>
       </div>
     </div>
