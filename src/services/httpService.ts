7abfb6634904f6525813622c9cc792b266f840d0
RanGojo <rangojo@rango.exchange>
2022-10-29 13:46:18 +0300

    customizable base url for the API, added name to token name

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
+const baseURL =
+  typeof process !== 'undefined' && process.env.REACT_APP_RANGO_BASE_URL
+    ? process.env.REACT_APP_RANGO_BASE_URL
+    : 'https://api.rango.exchange'
+
 export const httpService = axios.create({
-  baseURL: 'https://api.rango.exchange',
+  baseURL,
 })
--- src/types/api/meta.ts
+++ src/types/api/meta.ts
@@ -44,6 +44,7 @@ export type SwapperMetaDto = {
  * @property {string} blockchain - The blockchain which this token belongs to
  * @property {string | null} address - Smart contract address of token, null for native tokens
  * @property {string} symbol - The token symbol, e.g: ADA
+ * @property {string} symbol - The token name, e.g: Binance Pegged ETH
  * @property {number} decimals - Decimals of token in blockchain, example: 18
  * @property {string} image - Url of its image, example: https://api.rango.exchange/tokens/ETH/ETH.png
  *
@@ -52,6 +53,7 @@ export type Token = {
   blockchain: string
   address: string | null
   symbol: string
+  name: string
   decimals: number
   image: string
 }
