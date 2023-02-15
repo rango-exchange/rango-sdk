9d87a85ceb9afedfcb504a5f9cc2fdc612111fd2
Ikari-Shinji-re <120931880+Ikari-Shinji-re@users.noreply.github.com>
2023-02-15 13:14:37 +0330

    add request options (#5)
    * add request options
    
    * add request options to basic-sdk
    
    * update version
    
    ---------
    
    Co-authored-by: RanGojo <rangojo@rango.exchange>

--- packages/rango-sdk/src/services/client.ts
+++ packages/rango-sdk/src/services/client.ts
@@ -12,6 +12,7 @@ import {
   CreateTransactionResponse,
   ReportTransactionRequest,
   WalletDetailsResponse,
+  RequestOptions,
 } from '../types'
 
 type WalletAddresses = { blockchain: string; address: string }[]
@@ -41,61 +42,73 @@ export class RangoClient {
     }
   }
 
-  public async getAllMetadata(): Promise<MetaResponse> {
+  public async getAllMetadata(options?: RequestOptions): Promise<MetaResponse> {
     const axiosResponse = await httpService.get<MetaResponse>(
-      `/meta?apiKey=${this.apiKey}`
+      `/meta?apiKey=${this.apiKey}`,
+      { ...options }
     )
     return axiosResponse.data
   }
 
   public async getBestRoute(
-    requestBody: BestRouteRequest
+    requestBody: BestRouteRequest,
+    options?: RequestOptions
   ): Promise<BestRouteResponse> {
     const axiosResponse = await httpService.post<BestRouteResponse>(
       `/routing/best?apiKey=${this.apiKey}`,
       requestBody,
-      { headers: { 'X-Rango-Id': this.deviceId } }
+      { headers: { 'X-Rango-Id': this.deviceId }, ...options }
     )
     return axiosResponse.data
   }
 
   public async checkApproval(
-    requestId: string
+    requestId: string,
+    options?: RequestOptions
   ): Promise<CheckApprovalResponse> {
     const axiosResponse = await httpService.get<CheckApprovalResponse>(
-      `/tx/${requestId}/check-approval?apiKey=${this.apiKey}`
+      `/tx/${requestId}/check-approval?apiKey=${this.apiKey}`,
+      { ...options }
     )
     return axiosResponse.data
   }
 
   public async checkStatus(
-    requestBody: CheckTxStatusRequest
+    requestBody: CheckTxStatusRequest,
+    options?: RequestOptions
   ): Promise<TransactionStatusResponse> {
     const axiosResponse = await httpService.post<TransactionStatusResponse>(
       `/tx/check-status?apiKey=${this.apiKey}`,
-      requestBody
+      requestBody,
+      { ...options }
     )
     return axiosResponse.data
   }
 
   public async createTransaction(
-    requestBody: CreateTransactionRequest
+    requestBody: CreateTransactionRequest,
+    options?: RequestOptions
   ): Promise<CreateTransactionResponse> {
     const axiosResponse = await httpService.post<CreateTransactionResponse>(
       `/tx/create?apiKey=${this.apiKey}`,
-      requestBody
+      requestBody,
+      { ...options }
     )
     return axiosResponse.data
   }
 
   public async reportFailure(
-    requestBody: ReportTransactionRequest
+    requestBody: ReportTransactionRequest,
+    options?: RequestOptions
   ): Promise<void> {
-    await httpService.post(`/tx/report-tx?apiKey=${this.apiKey}`, requestBody)
+    await httpService.post(`/tx/report-tx?apiKey=${this.apiKey}`, requestBody, {
+      ...options,
+    })
   }
 
   public async getWalletsDetails(
-    walletAddresses: WalletAddresses
+    walletAddresses: WalletAddresses,
+    options?: RequestOptions
   ): Promise<WalletDetailsResponse> {
     let walletAddressesQueryParams = ''
     for (let i = 0; i < walletAddresses.length; i++) {
@@ -103,7 +116,8 @@ export class RangoClient {
       walletAddressesQueryParams += `&address=${walletAddress.blockchain}.${walletAddress.address}`
     }
     const axiosResponse = await httpService.get<WalletDetailsResponse>(
-      `/wallets/details?apiKey=${this.apiKey}${walletAddressesQueryParams}`
+      `/wallets/details?apiKey=${this.apiKey}${walletAddressesQueryParams}`,
+      { ...options }
     )
     return axiosResponse.data
   }
