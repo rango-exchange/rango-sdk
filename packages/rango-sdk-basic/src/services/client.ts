de579d6ce8b63e4650dfdae4ba30b5bb725fc45c
RanGojo <99180934+RanGojo@users.noreply.github.com>
2023-02-28 12:14:40 +0300

    feat make API base url configurable (#7)
    * update rango-types version
    
    * make api url configurable
    
    * update version

--- packages/rango-sdk-basic/src/services/client.ts
+++ packages/rango-sdk-basic/src/services/client.ts
@@ -1,6 +1,5 @@
 import uuid from 'uuid-random'
 
-import { httpService } from './httpService'
 import {
   MetaResponse,
   QuoteRequest,
@@ -21,15 +20,18 @@ import {
 import { Signer } from 'ethers'
 import { executeEvmRoute as executeEvmRoute } from './executor'
 import { prettifyError } from '../utils/errors'
+import axios, { AxiosInstance } from 'axios'
 
 type WalletAddress = { blockchain: string; address: string }
 
 export class RangoClient {
   private readonly deviceId: string
-
   private readonly apiKey: string
+  private readonly apiUrl: string
+  private readonly httpService: AxiosInstance
 
-  constructor(apiKey: string, debug = false) {
+  constructor(apiKey: string, debug = false, apiUrl?: string) {
+    this.apiUrl = apiUrl || 'https://api.rango.exchange'
     this.apiKey = apiKey
     try {
       if (typeof window !== 'undefined') {
@@ -47,12 +49,15 @@ export class RangoClient {
     } catch (e) {
       this.deviceId = uuid()
     }
+    this.httpService = axios.create({
+      baseURL: this.apiUrl,
+    })
     if (debug) {
-      httpService.interceptors.request.use((request) => {
+      this.httpService.interceptors.request.use((request) => {
         console.log('Starting Request', JSON.stringify(request, null, 2))
         return request
       })
-      httpService.interceptors.response.use((response) => {
+      this.httpService.interceptors.response.use((response) => {
         console.log('Response:', JSON.stringify(response, null, 2))
         return response
       })
@@ -60,7 +65,7 @@ export class RangoClient {
   }
 
   public async meta(options?: RequestOptions): Promise<MetaResponse> {
-    const axiosResponse = await httpService.get<MetaResponse>(
+    const axiosResponse = await this.httpService.get<MetaResponse>(
       `/basic/meta?apiKey=${this.apiKey}`,
       { ...options }
     )
@@ -68,7 +73,7 @@ export class RangoClient {
   }
 
   public async chains(options?: RequestOptions): Promise<BlockchainMeta[]> {
-    const axiosResponse = await httpService.get<BlockchainMeta[]>(
+    const axiosResponse = await this.httpService.get<BlockchainMeta[]>(
       `/basic/meta/blockchains?apiKey=${this.apiKey}`,
       { ...options }
     )
@@ -76,7 +81,7 @@ export class RangoClient {
   }
 
   public async swappers(options?: RequestOptions): Promise<SwapperMeta[]> {
-    const axiosResponse = await httpService.get<SwapperMeta[]>(
+    const axiosResponse = await this.httpService.get<SwapperMeta[]>(
       `/basic/meta/swappers?apiKey=${this.apiKey}`,
       { ...options }
     )
@@ -86,10 +91,11 @@ export class RangoClient {
   public async messagingProtocols(
     options?: RequestOptions
   ): Promise<MessagingProtocolsResponse> {
-    const axiosResponse = await httpService.get<MessagingProtocolsResponse>(
-      `/basic/meta/messaging-protocols?apiKey=${this.apiKey}`,
-      { ...options }
-    )
+    const axiosResponse =
+      await this.httpService.get<MessagingProtocolsResponse>(
+        `/basic/meta/messaging-protocols?apiKey=${this.apiKey}`,
+        { ...options }
+      )
     return axiosResponse.data
   }
 
@@ -111,7 +117,7 @@ export class RangoClient {
           ? quoteRequest.messagingProtocols.join(',')
           : undefined,
     }
-    const axiosResponse = await httpService.get<QuoteResponse>(
+    const axiosResponse = await this.httpService.get<QuoteResponse>(
       `/basic/quote?apiKey=${this.apiKey}`,
       {
         params: body,
@@ -127,7 +133,7 @@ export class RangoClient {
     txId: string,
     options?: RequestOptions
   ): Promise<CheckApprovalResponse> {
-    const axiosResponse = await httpService.get<CheckApprovalResponse>(
+    const axiosResponse = await this.httpService.get<CheckApprovalResponse>(
       `/basic/is-approved?apiKey=${this.apiKey}`,
       {
         params: { requestId, txId },
@@ -142,7 +148,7 @@ export class RangoClient {
     statusRequest: StatusRequest,
     options?: RequestOptions
   ): Promise<StatusResponse> {
-    const axiosResponse = await httpService.get<StatusResponse>(
+    const axiosResponse = await this.httpService.get<StatusResponse>(
       `/basic/status?apiKey=${this.apiKey}`,
       {
         params: statusRequest,
@@ -171,7 +177,7 @@ export class RangoClient {
           ? swapRequest.messagingProtocols.join(',')
           : undefined,
     }
-    const axiosResponse = await httpService.get<SwapResponse>(
+    const axiosResponse = await this.httpService.get<SwapResponse>(
       `/basic/swap?apiKey=${this.apiKey}`,
       {
         params: body,
@@ -186,7 +192,7 @@ export class RangoClient {
     requestBody: ReportTransactionRequest,
     options?: RequestOptions
   ): Promise<void> {
-    await httpService.post(
+    await this.httpService.post(
       `/basic/report-tx?apiKey=${this.apiKey}`,
       requestBody,
       {
@@ -200,7 +206,7 @@ export class RangoClient {
     walletAddress: WalletAddress,
     options?: RequestOptions
   ): Promise<WalletDetailsResponse> {
-    const axiosResponse = await httpService.get<WalletDetailsResponse>(
+    const axiosResponse = await this.httpService.get<WalletDetailsResponse>(
       `/basic/balance?apiKey=${this.apiKey}`,
       {
         params: walletAddress,
