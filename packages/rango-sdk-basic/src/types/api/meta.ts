403706c7ec086ee557d531fe58e2a4b04b03f17e
RanGojo <99180934+RanGojo@users.noreply.github.com>
2023-07-29 19:33:46 +0300

    feat: added meta request params (#12)
    * feat: added meta request params
    
    * fix: fix bug in meta params

--- packages/rango-sdk-basic/src/types/api/meta.ts
+++ packages/rango-sdk-basic/src/types/api/meta.ts
@@ -1,2 +1,3 @@
 export * from 'rango-types/lib/api/basic/meta'
 export * from 'rango-types/lib/api/shared/type-gaurds'
+export type { MetaRequest } from 'rango-types/lib/api/shared/meta'
--- packages/rango-sdk/src/types/api/meta.ts
+++ packages/rango-sdk/src/types/api/meta.ts
@@ -1,2 +1,3 @@
 export * from 'rango-types/lib/api/main/meta'
 export * from 'rango-types/lib/api/shared/type-gaurds'
+export type { MetaRequest } from 'rango-types/lib/api/shared/meta'
